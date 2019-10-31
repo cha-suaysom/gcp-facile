75087c331f31df235c5b2e6eb61b352e1e151286
cha-suaysom <nsuaysom@uw.edu>
2019-10-30 19:02:14 -0700

    Fix standardize

--- client.py
+++ client.py
@@ -8,19 +8,11 @@ import server_tools_pb2_grpc
 from google.protobuf import empty_pb2
 
 PORT = '50051'
-VALSPLIT = 0.2
-np.random.seed(5)
-Nrhs = 10000
 
-
-def run_facile(host_IP):
-    # Get a handle to the server
-    options = [('grpc.max_receive_message_length', 500*1024*1024 )]
-    #print("Max message length is ", grpc.max_message_length)
-    channel = grpc.insecure_channel(host_IP + ':' + PORT, options = options)
-    stub = server_tools_pb2_grpc.MnistServerStub(channel)
-
+def run_facile(channel, data_send, num_data_send):
+    start_time = time.time()
     # Get a client ID which you need to talk to the server
+    stub = server_tools_pb2_grpc.FacileServerStub(channel)
     try:
         logging.info("Request client id from server")
         response = stub.RequestClientID(server_tools_pb2.NullParam())
@@ -31,41 +23,56 @@ def run_facile(host_IP):
         return
     client_id = response.new_id
     logging.info("Client id is " + str(client_id))
+    finish_time = time.time()-start_time
 
-    X = pd.read_pickle("input/X_HB.pkl")[:Nrhs]
-    Y = pd.read_pickle("input/Y_HB.pkl")[:Nrhs]
-
-    x_input = X.to_json().encode('utf-8')
-    y_input = Y.to_json().encode('utf-8')
-
+    print("Time establishing server connections ", finish_time)
     # Send the data to the server and receive an answer
     start_time = time.time()
-    logging.info("Number tested is " + str(Nrhs))
+    logging.info("Number tested is " + str(num_data_send))
     logging.info("Submitting files and waiting")
     data_message = server_tools_pb2.DataMessage(
-        client_id=client_id, x_input=x_input, y_input=y_input, batch_size=32)
+        client_id=client_id, data=data_send, batch_size=32)
     response = stub.StartJobWait(data_message, 100, [])
 
     # Print output
     whole_time = time.time() - start_time
-    # print("Predict time:", response.infer_time)
-    # print("Fraction of time spent not predicting:",
-    #       (1 - response.infer_time / whole_time) * 100, '%')
-    #print(response.prediction)
+    print("Predict time:", response.infer_time)
+    print("Fraction of time spent not predicting:",
+          (1 - response.infer_time / whole_time) * 100, '%')
     A = np.frombuffer(response.prediction,dtype = np.float32)
-    #print(A.shape)
     print(list(np.frombuffer(response.prediction,dtype = np.float32))[:10])
-    print(X[:10])
     channel.close()
 
+
+def setup_server(host_IP):
+    start_time = time.time()
+    options = [('grpc.max_receive_message_length', 500*1024*1024 )]
+    channel = grpc.insecure_channel(host_IP + ':' + PORT, options = options)
+    
+    finish_time = time.time()-start_time
+    print("Time defining server stub is ", finish_time)
+    return channel
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser()
     parser.add_argument('--IP', type=str, default="localhost")
+    parser.add_argument('--num_send', type=int, default="localhost")
+
     args = parser.parse_args()
     logging.basicConfig()
     logging.root.setLevel(logging.NOTSET)
     logging.basicConfig(level=logging.NOTSET)
-    for i in range(1):
-        run_facile(args.IP)
+
+    start_time = time.time()
+    read_rec_hit = pd.read_pickle("input/X_HB.pkl")[:args.num_send]
+    print(len(read_rec_hit))
+    finish_time = time.time()-start_time
+    print("Time reading data from local file (pkl->pandas) is ", finish_time)
+
+    start_time = time.time()
+    compressed_data = read_rec_hit.to_json().encode('utf-8')
+    finish_time = time.time()-start_time
+    print("Time reading data from local file (pandas->bytes) is ", finish_time)
+    for i in range(5):
+        run_facile(setup_server(args.IP), compressed_data, args.num_send)
--- server.py
+++ server.py
@@ -14,6 +14,7 @@
 
 from keras.layers import Dense, BatchNormalization, Input, Dropout
 from keras.layers import Activation, concatenate, GRU, Dropout
+from keras.models import load_model
 import keras.backend as K
 from concurrent import futures
 import logging
@@ -26,9 +27,11 @@ import server_tools_pb2
 import server_tools_pb2_grpc
 import pandas as pd
 from keras.models import Model
+import tensorflow as tf
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 PORT = '50051'
+tf.logging.set_verbosity(tf.logging.ERROR)
 
 global processes, max_id, results, max_client_ids
 global max_client_id, new_client_permitted, times
@@ -41,28 +44,13 @@ results = {}
 times = {}
 
 
-class Model4ExpLLLow(mc.ClassModel):
-    def get_outputs(self):
-        self.name = '4layersExpLLLow'
-        RATE = 0.001
-        self.epochs = 11
-        h = self.inputs
-        h = Dropout(rate=RATE)(h)
-        h = Dense(36, activation='relu')(h)
-        norm = Dropout(rate=RATE)(h)
-        h = Dense(11, activation='relu')(norm)
-        norm = Dropout(rate=RATE)(h)
-        h = Dense(3, activation='relu')(norm)
-        return Dense(1, activation='linear', name='output')(h)
-
-
 def verify_request(request):
     logging.info("Client id is " + str(request.client_id))
     logging.info("Batch size is " + str(request.batch_size))
     return (request.client_id in max_client_ids) and request.batch_size > 0
 
 
-class MnistServer(server_tools_pb2_grpc.MnistServerServicer):
+class FacileServer(server_tools_pb2_grpc.FacileServerServicer):
     def StartJobWait(self, request, context):
         logging.info("StartJobWait begins")
         if not verify_request(request):
@@ -78,31 +66,21 @@ class MnistServer(server_tools_pb2_grpc.MnistServerServicer):
         """
         Initializing and standardizing X
         """
-        sample = mc.Sample()
-        sample.X = pd.read_json(request.x_input.decode('utf-8')) # OK!
-        sample.X.drop(['PU', 'pt'], 1, inplace=True)
-        sample.idx = np.random.permutation(sample.X.shape[0])
-        print('Standardizing...')
-        mu, std = np.mean(sample.X, axis=0), np.std(sample.X, axis=0)
-        sample.standardize(mu, std)
-        n_inputs = sample.X.shape[1]
+        #sample = mc.Sample()
+        X = pd.read_json(request.data.decode('utf-8')) # OK!
+        X.drop(['PU', 'pt'], 1, inplace=True)
 
-        # Define class model
-        model = Model4ExpLLLow(n_inputs, True) # True for GPU and false for CPU
-        model.load_model("weights.h5")
-        #sample.infer(model)
-        predictions, infer_time = model.predict(sample.X, 32)
+        # print('Standardizing...')
+        # mu, std = np.mean(sample.X, axis=0), np.std(sample.X, axis=0)
+        # sample.standardize(mu, std)
+
+        with tf.device('/gpu:0'):
+            weight_file = load_model("weights.h5")
+            start_time = time.time()
+            predictions = weight_file.predict(X, 32)
+            infer_time = time.time()-start_time
         # Need this otherwise two workers will be conflicted
         K.clear_session()
-        # print("------------PREDICTION-----------")
-        # print(predictions)
-        # print(type(predictions))
-        # print(predictions.tobytes())
-        # print(predictions.shape)
-        # print(predictions[:,0].shape)
-        # print(type(predictions[0]))
-        # print(predictions[:,0])
-        # print(predictions.dtype)
         return server_tools_pb2.PredictionMessage(
             complete=True,
             prediction=predictions[:,0].tobytes(),
@@ -126,8 +104,8 @@ class MnistServer(server_tools_pb2_grpc.MnistServerServicer):
 def serve():
     options = [('grpc.max_receive_message_length', 2047*1024*1024 )]
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10),options = options)
-    server_tools_pb2_grpc.add_MnistServerServicer_to_server(
-        MnistServer(), server)
+    server_tools_pb2_grpc.add_FacileServerServicer_to_server(
+        FacileServer(), server)
    
     server.add_insecure_port('[::]:' + PORT)
     server.start()
